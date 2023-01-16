2bab0172780fab2afa445d15e423c87f73959d87
Simon Goumaz <simon@attentif.ch>
2023-01-16 17:42:35 +0100

    Replace `yarn` with `just`

---
+++ .mocharc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  exit: true,
+  slow: 75,
+  timeout: 2000,
+  ui: 'bdd',
+  diff: true,
+  reporter: 'dot',
+  spec: 'test/**/*.test.js',
+  'watch-files': ['test/**/*.js']
+};
