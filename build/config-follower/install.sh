686108f950c5ef5cf616dbb2b87e283b2f6c1d34
Perki <perki@pryv.com>
2025-12-05 09:58:38 +0100

    updating for docker compose v2.40.2

--- build/config-follower/install.sh
+++ build/config-follower/install.sh
@@ -14,7 +14,7 @@ run rm -r /etc/service/sshd && rm /etc/my_init.d/00_regen_ssh_host_keys.sh
 run apt-get update
 run apt-get install -y sudo curl python-dev libffi-dev gcc libc-dev make
 # Docker-compose download and preparation
-run sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+run sudo curl -L "https://github.com/docker/compose/releases/download/v2.40.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 run sudo chmod +x /usr/local/bin/docker-compose
 run sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 # To disable password prompt for sudo
